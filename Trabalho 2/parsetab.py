8c4f8bdded3d562bb929a02e46a4c46643b806f9
Reginaldo Neto <reginaldoneto09@gmail.com>
2024-04-26 20:22:01 +0100

    Entrega com erro nas relações

--- Trabalho 2/parsetab.py
+++ Trabalho 2/parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'ABRE_PAR CARD_UM CARD_UM_VARIOS CARD_VARIOS CARD_ZERO_UM CARD_ZERO_VARIOS CLASSES DIAGRAMA DOIS_PONTOS FECHA_PAR MAIORQ MENORQ PONTO_VIRGULA PRIVACIDADE RELACOES REL_AGREGACAO_DIR REL_AGREGRACAO_ESQ REL_ASSOCIACAO_DIR REL_ASSOCIACAO_ESQ REL_COMPOSICAO_DIR REL_COMPOSICAO_ESQ REL_DEPENDENCIA_DIR REL_DEPENDENCIA_ESQ REL_HERANCA_DIR REL_HERANCA_ESQ REL_REALIZACAO_DIR REL_REALIZACAO_ESQ REL_SOLIDA REL_TRACEJADA RETA TIPO TITULO VARIAVEL VIRGULAprogram : titletitle : TITULO DOIS_PONTOS VARIAVEL'
+_lr_signature = 'ABRE_PAR ASPAS CARDINALIDADE DIAGRAMA DOIS_PONTOS FECHA_PAR GERAL LIST MAIORQ MENORQ PALAVRA_CLASSES PALAVRA_RELACOES PONTO_VIRGULA PRIVACIDADE RETA TIPO TIPOS_RELACAO TITULO VARIAVELprogram : comeco iniRelacoes iniClassescomeco : TITULO DOIS_PONTOS VARIAVEL DIAGRAMA GERAL DOIS_PONTOSiniRelacoes : MENORQ MENORQ PALAVRA_RELACOES relacoes MAIORQ MAIORQrelacoes : relacao\n    | relacoes relacaorelacao : VARIAVEL ABRE_PAR interno_relacao FECHA_PAR VARIAVEL PONTO_VIRGULA\n    interno_relacao : CARDINALIDADE TIPOS_RELACAO ASPAS VARIAVEL ASPAS TIPOS_RELACAO CARDINALIDADE\n                    | CARDINALIDADE TIPOS_RELACAO TIPOS_RELACAO CARDINALIDADE\n                    | TIPOS_RELACAO TIPOS_RELACAO\n    iniClasses : MENORQ MENORQ PALAVRA_CLASSES classes MAIORQ MAIORQclasses : classe\n    | classes classeclasse : VARIAVEL ABRE_PAR interno_classe FECHA_PAR PONTO_VIRGULA\n    interno_classe : atributos metodos_opt\n                   | metodos\n                   | empty\n    \n    metodos_opt : RETA metodos\n                | empty\n    empty :metodos : metodo\n    | metodos metodometodo : PRIVACIDADE VARIAVEL ABRE_PAR FECHA_PAR PONTO_VIRGULA\n              | PRIVACIDADE VARIAVEL ABRE_PAR VARIAVEL FECHA_PAR PONTO_VIRGULAatributos : atributo\n    | atributos atributoatributo : PRIVACIDADE TIPO DOIS_PONTOS VARIAVEL PONTO_VIRGULA\n                | PRIVACIDADE LIST MENORQ VARIAVEL MAIORQ DOIS_PONTOS VARIAVEL PONTO_VIRGULA'
     
-_lr_action_items = {'TITULO':([0,],[3,]),'$end':([1,2,5,],[0,-1,-2,]),'DOIS_PONTOS':([3,],[4,]),'VARIAVEL':([4,],[5,]),}
+_lr_action_items = {'TITULO':([0,],[3,]),'$end':([1,7,33,],[0,-1,-10,]),'MENORQ':([2,4,5,8,25,29,54,],[5,8,9,11,-2,-3,62,]),'DOIS_PONTOS':([3,18,52,74,],[6,25,60,77,]),'VARIAVEL':([6,12,14,15,16,19,20,23,27,40,41,51,57,58,60,61,62,63,77,],[10,17,21,17,-4,21,-11,-5,-12,53,55,53,65,-13,66,67,69,-6,79,]),'PALAVRA_RELACOES':([9,],[12,]),'DIAGRAMA':([10,],[13,]),'PALAVRA_CLASSES':([11,],[14,]),'GERAL':([13,],[18,]),'MAIORQ':([15,16,19,20,22,23,26,27,58,63,69,],[22,-4,26,-11,29,-5,33,-12,-13,-6,74,]),'ABRE_PAR':([17,21,53,],[24,28,61,]),'CARDINALIDADE':([24,56,75,],[31,64,78,]),'TIPOS_RELACAO':([24,31,32,42,70,],[32,42,43,56,75,]),'FECHA_PAR':([28,30,34,35,36,37,38,39,43,45,46,48,50,59,61,64,67,71,73,76,78,80,],[-19,41,44,-19,-15,-16,-24,-20,-9,-14,-25,-18,-21,-17,68,-8,72,-26,-22,-23,-7,-27,]),'PRIVACIDADE':([28,35,36,38,39,46,47,50,59,71,73,76,80,],[40,49,51,-24,-20,-25,51,-21,51,-26,-22,-23,-27,]),'RETA':([35,38,46,71,80,],[47,-24,-25,-26,-27,]),'TIPO':([40,49,],[52,52,]),'LIST':([40,49,],[54,54,]),'ASPAS':([42,65,],[57,70,]),'PONTO_VIRGULA':([44,55,66,68,72,79,],[58,63,71,73,76,80,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'program':([0,],[1,]),'title':([0,],[2,]),}
+_lr_goto_items = {'program':([0,],[1,]),'comeco':([0,],[2,]),'iniRelacoes':([2,],[4,]),'iniClasses':([4,],[7,]),'relacoes':([12,],[15,]),'relacao':([12,15,],[16,23,]),'classes':([14,],[19,]),'classe':([14,19,],[20,27,]),'interno_relacao':([24,],[30,]),'interno_classe':([28,],[34,]),'atributos':([28,],[35,]),'metodos':([28,47,],[36,59,]),'empty':([28,35,],[37,48,]),'atributo':([28,35,],[38,46,]),'metodo':([28,36,47,59,],[39,50,39,50,]),'metodos_opt':([35,],[45,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,6 +27,31 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> program","S'",1,None,None,None),
-  ('program -> title','program',1,'p_def_program','sagui_parser.py',175),
-  ('title -> TITULO DOIS_PONTOS VARIAVEL','title',3,'p_def_tile','sagui_parser.py',179),
+  ('program -> comeco iniRelacoes iniClasses','program',3,'p_def_program','sagui_parser.py',216),
+  ('comeco -> TITULO DOIS_PONTOS VARIAVEL DIAGRAMA GERAL DOIS_PONTOS','comeco',6,'p_def_comeco','sagui_parser.py',221),
+  ('iniRelacoes -> MENORQ MENORQ PALAVRA_RELACOES relacoes MAIORQ MAIORQ','iniRelacoes',6,'p_def_iniRelacoes','sagui_parser.py',225),
+  ('relacoes -> relacao','relacoes',1,'p_def_relacoes','sagui_parser.py',229),
+  ('relacoes -> relacoes relacao','relacoes',2,'p_def_relacoes','sagui_parser.py',230),
+  ('relacao -> VARIAVEL ABRE_PAR interno_relacao FECHA_PAR VARIAVEL PONTO_VIRGULA','relacao',6,'p_def_relacao','sagui_parser.py',237),
+  ('interno_relacao -> CARDINALIDADE TIPOS_RELACAO ASPAS VARIAVEL ASPAS TIPOS_RELACAO CARDINALIDADE','interno_relacao',7,'p_interno_relacao','sagui_parser.py',242),
+  ('interno_relacao -> CARDINALIDADE TIPOS_RELACAO TIPOS_RELACAO CARDINALIDADE','interno_relacao',4,'p_interno_relacao','sagui_parser.py',243),
+  ('interno_relacao -> TIPOS_RELACAO TIPOS_RELACAO','interno_relacao',2,'p_interno_relacao','sagui_parser.py',244),
+  ('iniClasses -> MENORQ MENORQ PALAVRA_CLASSES classes MAIORQ MAIORQ','iniClasses',6,'p_def_iniClasses','sagui_parser.py',262),
+  ('classes -> classe','classes',1,'p_def_classes','sagui_parser.py',266),
+  ('classes -> classes classe','classes',2,'p_def_classes','sagui_parser.py',267),
+  ('classe -> VARIAVEL ABRE_PAR interno_classe FECHA_PAR PONTO_VIRGULA','classe',5,'p_def_classe','sagui_parser.py',274),
+  ('interno_classe -> atributos metodos_opt','interno_classe',2,'p_interno_classe','sagui_parser.py',279),
+  ('interno_classe -> metodos','interno_classe',1,'p_interno_classe','sagui_parser.py',280),
+  ('interno_classe -> empty','interno_classe',1,'p_interno_classe','sagui_parser.py',281),
+  ('metodos_opt -> RETA metodos','metodos_opt',2,'p_metodos_opt','sagui_parser.py',292),
+  ('metodos_opt -> empty','metodos_opt',1,'p_metodos_opt','sagui_parser.py',293),
+  ('empty -> <empty>','empty',0,'p_empty','sagui_parser.py',301),
+  ('metodos -> metodo','metodos',1,'p_def_metodos','sagui_parser.py',305),
+  ('metodos -> metodos metodo','metodos',2,'p_def_metodos','sagui_parser.py',306),
+  ('metodo -> PRIVACIDADE VARIAVEL ABRE_PAR FECHA_PAR PONTO_VIRGULA','metodo',5,'p_def_metodo','sagui_parser.py',313),
+  ('metodo -> PRIVACIDADE VARIAVEL ABRE_PAR VARIAVEL FECHA_PAR PONTO_VIRGULA','metodo',6,'p_def_metodo','sagui_parser.py',314),
+  ('atributos -> atributo','atributos',1,'p_def_atributos','sagui_parser.py',321),
+  ('atributos -> atributos atributo','atributos',2,'p_def_atributos','sagui_parser.py',322),
+  ('atributo -> PRIVACIDADE TIPO DOIS_PONTOS VARIAVEL PONTO_VIRGULA','atributo',5,'p_def_atributo','sagui_parser.py',329),
+  ('atributo -> PRIVACIDADE LIST MENORQ VARIAVEL MAIORQ DOIS_PONTOS VARIAVEL PONTO_VIRGULA','atributo',8,'p_def_atributo','sagui_parser.py',330),
 ]
--- Trabalho 2/sagui_parser.py
+++ Trabalho 2/sagui_parser.py
@@ -1,209 +1,200 @@
-# TITULO: IPB
-# DIAGRAMA GERAL:
-# <<RELACOES		
-#     Course (0* --o 1) School;
-#     Course (1 o-- 0*) Subject;
-#     Student (1 -"attends"- 0*) Course;
-#     Subject (0*-- ) Grade ( -- 0*) Student;
-#     Teacher (0* --"teaches"--1) Subject;
-#     person (<|--) Teacher;
-#     person (<|--) Student;
-#     School (1 *--> 1*) Teacher;
-# >>
-# <<CLASSES
-#     School(
-#         PRI STRING: name;
-#         PRI LIST<Course>: coursesCollection;
-#         PRI LIST<Teacher>: teachersCollection;
-#         |
-#         PUB addCourse(); 
-#         PUB addTeacher();
-#     );
-#     Course(
-#         PRI STRING: name;
-#         PRI LIST<Student>: studentsCollection;
-#         PRI LIST<Subject>: subjectsCollection;
-#     );
-#     Grade(
-#         PRI INT: value, subject_code, student_number;
-#         |
-#         PUB INT: getValue();
-#         PUB VOID: setValue();
-#     );
-#     Teacher(
-#         PRI LIST<Subject>: subjectCollection;
-#     );
-#     Student(
-#         PRI INT: number;
-#         PRI STRING: course_name;
-#         PRI LIST<Grade>: gradesCollection;
-#         );
-#     Subject(
-#         PRI STRING: name, course_name, teacher_name;
-#         PRI INT: code;
-#         PRI LIST<Grade>: gradesCollection;
-#     );
-#     person(
-#         PRI STRING: name;
-#     );
-# >>
-
 tokens = (
-    'TITULO', 'VARIAVEL', 'DIAGRAMA', 'CLASSES', 'RELACOES',
-    'VIRGULA', 'ABRE_PAR', 'FECHA_PAR', 'RETA', 'MAIORQ', 'MENORQ', 'DOIS_PONTOS', 'PONTO_VIRGULA',
-    #'INT', 'FLOAT', 'LIST', 'VOID', 'STRING', 
-    'TIPO', 'PRIVACIDADE',
-    #'PUB', 'PRI', 'PRO', 'PAC', 
-    'CARD_UM', 'CARD_VARIOS', 'CARD_ZERO_UM', 'CARD_ZERO_VARIOS', 'CARD_UM_VARIOS',
-    'REL_SOLIDA', 'REL_TRACEJADA', 
-    'REL_HERANCA_ESQ', 'REL_HERANCA_DIR',
-    'REL_COMPOSICAO_ESQ', 'REL_COMPOSICAO_DIR', 
-    'REL_AGREGRACAO_ESQ', 'REL_AGREGACAO_DIR',
-    'REL_ASSOCIACAO_ESQ', 'REL_ASSOCIACAO_DIR', 
-    'REL_DEPENDENCIA_ESQ', 'REL_DEPENDENCIA_DIR',
-    'REL_REALIZACAO_ESQ', 'REL_REALIZACAO_DIR', 
+    'TITULO',  'DIAGRAMA', 'GERAL', 'PALAVRA_CLASSES', 'PALAVRA_RELACOES',
+    'ABRE_PAR', 'FECHA_PAR', 'RETA', 'MAIORQ', 'MENORQ', 'DOIS_PONTOS', 'PONTO_VIRGULA', 'VARIAVEL',
+    'TIPO', 'LIST', 'PRIVACIDADE', 'CARDINALIDADE', 'TIPOS_RELACAO', 'ASPAS'
+    # 'CARD_UM', 'CARD_VARIOS', 'CARD_ZERO_UM', 'CARD_ZERO_VARIOS', 'CARD_UM_VARIOS',
+    # 'REL_SOLIDA', 'REL_TRACEJADA', 
+    # 'REL_HERANCA_ESQ', 'REL_HERANCA_DIR',
+    # 'REL_COMPOSICAO_ESQ', 'REL_COMPOSICAO_DIR', 
+    # 'REL_AGREGRACAO_ESQ', 'REL_AGREGACAO_DIR',
+    # 'REL_ASSOCIACAO_ESQ', 'REL_ASSOCIACAO_DIR', 
+    # 'REL_DEPENDENCIA_ESQ', 'REL_DEPENDENCIA_DIR',
+    # 'REL_REALIZACAO_ESQ', 'REL_REALIZACAO_DIR', 
 )
 literals = ",();><|-*"
 
 #AS PALAVRAS RESERVADAS DEVEM SER FEITAS PRIMEIRO
+def t_ASPAS (t):
+    r'\"'
+    return t
+
 def t_TITULO(t): 
     r'TITULO'
     return t
 
 def t_DIAGRAMA(t): 
-    r'DIAGRAMA GERAL'
+    r'DIAGRAMA'
     return t
 
-def t_TIPO(t): 
-    r'INT|FLOAT|STRING|LIST|VOID'
+def t_GERAL(t): 
+    r'GERAL'
     return t
 
-# def t_INT(t): 
-#     r'INT'
-#     return t
-
-# def t_FLOAT(t): 
-#     r'FLOAT'
-#     return t
-
-# def t_STRING(t): 
-#     r'STRING'
-#     return t
-
-# def t_VOID(t): 
-#     r'VOID'
-#     return t
-
-# def t_LIST(t): 
-#     r'LIST'
-#     return t
-
-def t_PRIVACIDADE(t): 
-    r'PUB|PRI|PAC|PRO'
-    return t
-
-# def t_PUB(t): 
-#     r'PUB'
-#     return t
-
-# def t_PRI(t): 
-#     r'PRI'
-#     return t
-
-# def t_PRO(t): 
-#     r'PRO'
-#     return t
-
-# def t_PAC(t): 
-#     r'PAC'
-#     return t
-
-def t_CLASSES(t): 
+def t_PALAVRA_CLASSES(t): 
     r'CLASSES'
     return t
 
-def t_RELACOES(t): 
+def t_PALAVRA_RELACOES(t): 
     r'RELACOES'
     return t
 
-def t_CARD_UM_VARIOS(t): 
-    r'1\*'
+def t_INT(t):
+    r'INT'
+    t.value = "int"
+    t.type = "TIPO"
     return t
 
-t_CARD_ZERO_VARIOS = r'0\*'
+def t_FLOAT(t):
+    r'FLOAT'
+    t.value = "float"
+    t.type = "TIPO"
+    return t
 
-def t_CARD_ZERO_UM(t): 
-    r'01'
+def t_STRING(t): 
+    r'STRING'
+    t.value="String"
+    t.type = "TIPO"
+    return t
+
+def t_VOID(t): 
+    r'VOID'
+    t.value="void"
+    t.type = "TIPO"
+    return t
+
+def t_LIST(t): 
+    r'LIST'
+    return t
+
+def t_PUB(t): 
+    r'PUB'
+    t.type = "PRIVACIDADE"
+    t.value = "+"
+    return t
+
+def t_PRI(t): 
+    r'PRI'
+    t.type = "PRIVACIDADE"
+    t.value = "-"
+    return t
+
+def t_PRO(t): 
+    r'PRO'
+    t.type = "PRIVACIDADE"
+    t.value = "#"
+    return t
+
+def t_PAC(t): 
+    r'PAC'
+    t.type = "PRIVACIDADE"
+    t.value = "~"
     return t
 
 def t_CARD_UM(t): 
     r'1'
+    t.type = "CARDINALIDADE"
     return t
 
-t_CARD_VARIOS = r'\*'
+def t_CARD_UM_VARIOS(t): 
+    r'1\*'
+    t.type = "CARDINALIDADE"
+    return t
+
+def t_CARD_ZERO_VARIOS (t):
+    r'0\*'
+    t.type = "CARDINALIDADE"
+    return t
+
+def t_CARD_ZERO_UM(t): 
+    r'01'
+    t.type = "CARDINALIDADE"
+    return t
+
+def t_CARD_VARIOS (t):
+    r'\*'
+    t.type = "CARDINALIDADE"
+    return t
 
 def t_REL_HERANCA_ESQ(t):
-    r'<|\-'
+    r'<\|\-'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_REL_HERANCA_DIR(t):
-    r'\-|>'
+    r'\-\|>'
+    t.type = "TIPOS_RELACAO"
     return t
 
-t_REL_COMPOSICAO_ESQ = r'\*\-'
+def t_REL_COMPOSICAO_ESQ(t):
+    r'\*\-'
+    t.type = "TIPOS_RELACAO"
+    return t
 
-t_REL_COMPOSICAO_DIR = r'\-\*'
+def t_REL_COMPOSICAO_DIR (t):
+    r'\-\*'
+    t.type = "TIPOS_RELACAO"
+    return t
 
 def t_REL_AGREGACAO_ESQ(t):
-    r'o-'
+    r'o\-'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_REL_AGREGACAO_DIR(t):
-    r'-o'
+    r'\-o'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_REL_ASSOCIACAO_ESQ(t):
-    r'<-'
+    r'<\-'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_REL_ASSOCIACAO_DIR(t):
-    r'->'
+    r'\->'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_REL_DEPENDENCIA_ESQ(t):
-    r'<.'
+    r'<\.'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_REL_DEPENDENCIA_DIR(t):
-    r'.>'
+    r'\.>'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_REL_REALIZACAO_ESQ(t):
-    r'<|.'
+    r'<\|\.'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_REL_REALIZACAO_DIR(t):
-    r'.|>'
+    r'\.\|>'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_REL_SOLIDA(t):
-    r'-'
+    r'\-'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_REL_TRACEJADA(t):
-    r'.'
+    r'\.'
+    t.type = "TIPOS_RELACAO"
     return t
 
 def t_VARIAVEL(t):
-    r'[a-zA-Z_]+'
+    r'[a-zA-Z_0-9]+'
     return t
+
 
 t_DOIS_PONTOS = r':'
 t_PONTO_VIRGULA = r'\;'
 t_RETA = r'\|'
-t_MAIORQ = r'\>'
-t_MENORQ = r'\<'
-t_VIRGULA = r'\,'
+t_MAIORQ = r'>'
+t_MENORQ = r'<'
 t_ABRE_PAR = r'\('
 t_FECHA_PAR = r'\)'
 t_ignore = " \t\n"
@@ -222,16 +213,54 @@ lexer = lex.lex()
 # Expansão das regras de análise sintática
 
 def p_def_program(p):
-    'program : comeco iniClasses relacoes'
-    p[0] = f"{p[1]}"
+    'program : comeco iniRelacoes iniClasses'
+    p[0] = f"{p[1]}{p[2]}"
+    print(p[0])
     
 def p_def_comeco(p):
-    'comeco : TITULO DOIS_PONTOS VARIAVEL DIAGRAMA DOIS_PONTOS'
-    p[0] = f"%%{p[1]}{p[2]}{p[3]}\nclassDiagram\n\t"
+     'comeco : TITULO DOIS_PONTOS VARIAVEL DIAGRAMA GERAL DOIS_PONTOS'
+     p[0] = f"%%{p[1]}{p[2]}{p[3]}\nclassDiagram\n\t"
 
+def p_def_iniRelacoes(p):
+    'iniRelacoes : MENORQ MENORQ PALAVRA_RELACOES relacoes MAIORQ MAIORQ'
+    p[0]= p[4]
+    
+def p_def_relacoes(p):
+    '''relacoes : relacao
+    | relacoes relacao'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = p[1] + p[2]
+        
+def p_def_relacao(p):
+    'relacao : VARIAVEL ABRE_PAR interno_relacao FECHA_PAR VARIAVEL PONTO_VIRGULA'
+    p[0] = f"{p[1]}{p[3]}{p[5]}\n"  
+    
+def p_interno_relacao(p):
+    '''
+    interno_relacao : CARDINALIDADE TIPOS_RELACAO ASPAS VARIAVEL ASPAS TIPOS_RELACAO CARDINALIDADE
+                    | CARDINALIDADE TIPOS_RELACAO TIPOS_RELACAO CARDINALIDADE
+                    | TIPOS_RELACAO TIPOS_RELACAO
+    '''
+    if len(p) == 5:
+        p[0] = '\"' + p[1] + '\" ' + p[2] + ' \"' + p[3] + '\"' 
+    elif len(p)==3:
+        p[0] = p[1]
+    else:
+        p[0] = '\"' + p[1] + '\" ' + p[2] + p[6] + ' \"' + p[7] + '\"'  
+        
+
+
+
+
+
+
+        
+    
 def p_def_iniClasses(p):
-    'iniClasses : MENORQ MENORQ classes MAIORQ MAIORQ'
-    p[0]= p[3]
+    'iniClasses : MENORQ MENORQ PALAVRA_CLASSES classes MAIORQ MAIORQ'
+    p[0]= p[4]
     
 def p_def_classes(p):
     '''classes : classe
@@ -243,20 +272,71 @@ def p_def_classes(p):
     
 def p_def_classe(p):
     'classe : VARIAVEL ABRE_PAR interno_classe FECHA_PAR PONTO_VIRGULA'
-    p[0] = f"class {p[1]}","{",f"{p[3]}","}" 
-    #TA ERRADO, PRECISO RETORNAR O NOME DA CLASSE JUNTO COM OS ATRIBUTOS E METODOS
-    
-def p_def_interno_classe(p):
-    '''interno_classe : atributos
-    | interno_classe metodos
-    | interno_classe atributos RETA metodos'''
-    
+    p[0] = f"class {p[1]} {{\n{p[3]}\n}}\n"  
+     
+def p_interno_classe(p):
+    '''
+    interno_classe : atributos metodos_opt
+                   | metodos
+                   | empty
+    '''
+    if len(p) == 3:
+        p[0] = p[1] + '\n' + p[2] if p[2] else p[1]  # Concatena atributos e métodos se ambos existirem
+    elif len(p) == 2:
+        p[0] = p[1]  # Apenas métodos ou apenas atributos
+    else:
+        p[0] = ''    # Classe vazia
+
+def p_metodos_opt(p):
+    '''
+    metodos_opt : RETA metodos
+                | empty
+    '''
+    if len(p) == 3:
+        p[0] = p[2]  # Retorna os métodos se eles existirem
+    else:
+        p[0] = ''    # Retorna string vazia se não existirem métodos
+
+def p_empty(p):
+    'empty :'
+    pass
+
+def p_def_metodos(p):
+    '''metodos : metodo
+    | metodos metodo'''
     if len(p) == 2:
         p[0] = p[1]
     else:
-        p[0] = p[1] + p[2]
+        p[0] = p[1] + '\n' + p[2]
 
+def p_def_metodo(p):
+    '''metodo : PRIVACIDADE VARIAVEL ABRE_PAR FECHA_PAR PONTO_VIRGULA
+              | PRIVACIDADE VARIAVEL ABRE_PAR VARIAVEL FECHA_PAR PONTO_VIRGULA'''
+    if len(p)==6:
+        p[0] = f"{p[1]}{p[2]}();\n"
+    else:
+        p[0] = f"{p[1]}{p[2]}({p[4]});\n" 
+
+def p_def_atributos(p):
+    '''atributos : atributo
+    | atributos atributo'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = p[1] + '\n' + p[2]
+    
+def p_def_atributo(p):
+    '''atributo : PRIVACIDADE TIPO DOIS_PONTOS VARIAVEL PONTO_VIRGULA
+                | PRIVACIDADE LIST MENORQ VARIAVEL MAIORQ DOIS_PONTOS VARIAVEL PONTO_VIRGULA'''
+    if len(p)==6:
+        p[0] = f"{p[1]}{p[2]} {p[4]};\n"
+    else:
+        p[0] = f"{p[1]}List< {p[4]} > {p[7]};\n" 
+    
+        
 def p_error(p):
+    
+    print("LINHA '%s'" % p.lineno)
     print("Syntax error at '%s'" % p.value)
     
     
